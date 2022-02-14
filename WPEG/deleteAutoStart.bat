60ee7bfdb0e5f3bec9e6355b234dd34f093c5379
DuGuYifei <liuyifei97@163.com>
2022-02-14 15:50:42 +0100

    newversion

---
+++ WPEG/deleteAutoStart.bat
@@ -0,0 +1,2 @@
+@ehco off
+del /q/f "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\WallpaperEngineStart.lnk"
--- WallpaperEngine/WallpaperEngine/wallpaper.cpp
+++ WallpaperEngine/WallpaperEngine/wallpaper.cpp
@@ -167,50 +167,50 @@ std::wstring s2ws(const std::string& s)
 	return r;
 }
 
-// 程序开机自动启动
-void autostart(LPCWSTR strRegPath)
-{
-	HKEY hKey;
-
-	//1、找到系统的启动项  
-	if (RegOpenKeyEx(HKEY_CURRENT_USER, strRegPath, 0, KEY_ALL_ACCESS, &hKey) == ERROR_SUCCESS) ///打开启动项       
-	{
-		//2、得到本程序自身的全路径
-		TCHAR strExeFullDir[MAX_PATH];
-		GetModuleFileName(NULL, strExeFullDir, MAX_PATH);
-
-		//3、判断注册表项是否已经存在
-		TCHAR strDir[MAX_PATH] = {};
-		DWORD nLength = MAX_PATH;
-		long result = RegGetValue(hKey, nullptr, L"GISRestart", RRF_RT_REG_SZ, 0, strDir, &nLength);
-
-		//4、已经存在
-		if (result != ERROR_SUCCESS || _tcscmp(strExeFullDir, strDir) != 0)
-		{
-			//5、添加一个子Key,并设置值，"GISRestart"是应用程序名字（不加后缀.exe） 
-			RegSetValueEx(hKey, L"GISRestart", 0, REG_SZ, (LPBYTE)strExeFullDir, (lstrlen(strExeFullDir) + 1) * sizeof(TCHAR));
-
-			//6、关闭注册表
-			RegCloseKey(hKey);
-		}
-	}
-}
-
-// 取消开机自动启动
-void cancelAutoStart(LPCWSTR strRegPath)
-{
-	HKEY hKey;
-
-	//1、找到系统的启动项  
-	if (RegOpenKeyEx(HKEY_CURRENT_USER, strRegPath, 0, KEY_ALL_ACCESS, &hKey) == ERROR_SUCCESS)
-	{
-		//2、删除值
-		RegDeleteValue(hKey, L"GISRestart");
-
-		//3、关闭注册表
-		RegCloseKey(hKey);
-	}
-}
+//// 程序开机自动启动
+//void autostart(LPCWSTR strRegPath)
+//{
+//	HKEY hKey;
+//
+//	//1、找到系统的启动项  
+//	if (RegOpenKeyEx(HKEY_CURRENT_USER, strRegPath, 0, KEY_ALL_ACCESS, &hKey) == ERROR_SUCCESS) ///打开启动项       
+//	{
+//		//2、得到本程序自身的全路径
+//		TCHAR strExeFullDir[MAX_PATH];
+//		GetModuleFileName(NULL, strExeFullDir, MAX_PATH);
+//
+//		//3、判断注册表项是否已经存在
+//		TCHAR strDir[MAX_PATH] = {};
+//		DWORD nLength = MAX_PATH;
+//		long result = RegGetValue(hKey, nullptr, L"GISRestart", RRF_RT_REG_SZ, 0, strDir, &nLength);
+//
+//		//4、已经存在
+//		if (result != ERROR_SUCCESS || _tcscmp(strExeFullDir, strDir) != 0)
+//		{
+//			//5、添加一个子Key,并设置值，"GISRestart"是应用程序名字（不加后缀.exe） 
+//			RegSetValueEx(hKey, L"GISRestart", 0, REG_SZ, (LPBYTE)strExeFullDir, (lstrlen(strExeFullDir) + 1) * sizeof(TCHAR));
+//
+//			//6、关闭注册表
+//			RegCloseKey(hKey);
+//		}
+//	}
+//}
+//
+//// 取消开机自动启动
+//void cancelAutoStart(LPCWSTR strRegPath)
+//{
+//	HKEY hKey;
+//
+//	//1、找到系统的启动项  
+//	if (RegOpenKeyEx(HKEY_CURRENT_USER, strRegPath, 0, KEY_ALL_ACCESS, &hKey) == ERROR_SUCCESS)
+//	{
+//		//2、删除值
+//		RegDeleteValue(hKey, L"GISRestart");
+//
+//		//3、关闭注册表
+//		RegCloseKey(hKey);
+//	}
+//}
 LPCWSTR string2LPCWSTR(std::string str)
 {
 	size_t size = str.length();
@@ -282,11 +282,12 @@ int main(int argc, char* argv[])
 	//std::cout << ss << std::endl;
 	if (MessageBox(NULL, string2LPCWSTR("是否开机自启动(不想每次都开机设置桌面就选确定)"), string2LPCWSTR("WallpaperEngine"), MB_OKCANCEL) == 1)
 	{
-		system((addQuate((ss + "\\WallpaperEngineAutoStartUpCmd.bat")).c_str()));
+		system(addQuate((ss + "\\WallpaperEngineAutoStartUpCmd.bat")).c_str());
 	}
 	else
 	{
 		//cancelAutoStart(string2LPCWSTR(ss + "\\\\WallpaperEngineStart.exe"));
+		system(addQuate((ss + "\\deleteAutoStart.bat")).c_str());
 	}
 	
 	ss.append("\\\\ffplay.exe ");
